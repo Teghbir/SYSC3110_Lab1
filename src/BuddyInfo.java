39a14bed2bc213c5b1f2668db3be45b8b940666c
teghb <teghbir@gmail.com>
2024-09-20 13:09:40 -0400

    Removed an unnecessary display command

---
+++ src/BuddyInfo.java
@@ -0,0 +1,48 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    // Default constructor
+    public BuddyInfo() {
+        this.name = "Default Name";
+        this.address = "Default Address";
+        this.phoneNumber = "Default Phone Number";
+    }
+
+    // Parameterized constructor
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        AddressBook newAddyBook = new AddressBook();
+        // Create a BuddyInfo object
+        BuddyInfo buddy = new BuddyInfo("Homer", "742 Evergreen Terrace", "555-1234");
+
+
+        newAddyBook.addBuddy(buddy);
+        newAddyBook.displayBuddies();
+
+        newAddyBook.removeBuddy(buddy);
+        newAddyBook.displayBuddies();
+
+    }
+}
